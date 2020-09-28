047640c3032f3a1ab9d2740a38ad1c85b34baf94
yeezynaizi <howone1sky@gmail.com>
2020-09-28 20:14:05 +0300

    good drob

--- drob.py
+++ drob.py
@@ -1,22 +1,35 @@
+import time
+
+start = time.time()
+
+
 def to_10(n, x):
     st = 1
     z = 0
     q = 0
     n = str(n).split('.')
-    y = x ** len(n[1])
-    for i in range(len(n[1])-1, -1, -1):
-        if n[1][i].isdigit():            
-            z += int(n[1][i])* x * st
-        else:
-            z += (ord(n[1][i]) - 55) * x * st
-        st *= x
+    if len(n) == 2:
+        y = x ** len(n[1])
+        for i in range(len(n[1]) - 1, -1, -1):
+            if n[1][i].isdigit():
+                z += int(n[1][i]) * st
+            else:
+                z += (ord(n[1][i]) - 55) * st
+            st *= x
+    else:
+        y = 1
     st = 1
-    for i in range(len(n[0])-1, -1, -1):
-        if n[0][i].isdigit():            
-            q += int(n[0][i])* x * st
+    for i in range(len(n[0]) - 1, -1, -1):
+        if n[0][i].isdigit():
+            q += int(n[0][i]) * st
         else:
-            q += (ord(n[0][i]) - 55) * x * st
+            q += (ord(n[0][i]) - 55) * st
         st *= x
-    number = q + z/y
+    number = q + z / y
     print(z, y)
     print(number)
+
+
+to_10('1010.1010', 2)
+time.sleep(1)
+print("--- %s seconds ---" % (time.time() - start))

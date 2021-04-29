80d3b8220349b301d851c8af781245145b257dba
yeezy_na_izi <howone1sky@gmail.com>
2021-04-29 22:01:51 +0300

    refactor 114193

--- a_lot_of_figniya/114193.py
+++ a_lot_of_figniya/114193.py
@@ -9,13 +9,13 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 q = 0
-while x1 * y1 < x2 * y2:
-    q += 1
+while x1 * y2 < x2 * y1:
     x1 += 1
     y1 += 1
     gcd_n = gsd(x1, y1)
     x1 //= gcd_n
     y1 //= gcd_n
+    q += 1
 if x1 == x2 and y1 == y2:
     print(q)
 else:
